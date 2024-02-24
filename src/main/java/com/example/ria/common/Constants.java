03369833804e916655267009d68920e3e492d26d
Jungkihong <hnn07185@gmail.com>
2024-02-25 03:28:16 +0900

    정기홍 1-2 KaKaoDataMap 데이터 전달 api 구현 완료 - Clean 코드를 위하여 Constants.java 구현 ( public Map data도 적용 예정) - KaKao Data를 가져오기 위한 kakao_format 패키지 생성하여, VO들 생성 - PlaceList는 후에 데이터를 보여주기 위해, 수정하기 전 proto Type. 나중에 수정 - KaKaoMapService와 Controller prototype 제작 완료

---
+++ src/main/java/com/example/ria/common/Constants.java
@@ -0,0 +1,10 @@
+package com.example.ria.common;
+
+public class Constants {
+    public static final String KAKAO_SEARCH_API = "https://dapi.kakao.com";
+    public static final String KAKAO_SEARCH_PATH = "v2/local/search/keyword.json";
+
+    public static final String MAP_REDIRECT_URL = "https://map.kakao.com/link/map/";
+
+    public static final String SESSION_USER_ID = "session_user_id";
+}

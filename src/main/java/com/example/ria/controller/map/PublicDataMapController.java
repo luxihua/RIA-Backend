0ff5e59b6140db23fe2b11e8c60e5d641e458490
Jungkihong <hnn07185@gmail.com>
2024-02-29 15:31:55 +0900

    정기홍 1-3 PublicDataMap 데이터 전달 api 수정 완료 - Constants에 PublicDataAPI에 필요한 데이터 저장 - PublicDataAPi RestCliet 방식으로 가져오기 위해 PublicDataMapController.java과 PublicDataMapService 수정 - test 코드 수정

--- src/main/java/com/example/ria/controller/map/PublicDataMapController.java
+++ src/main/java/com/example/ria/controller/map/PublicDataMapController.java
@@ -1,11 +1,9 @@
 package com.example.ria.controller.map;
 
-import com.example.ria.dto.vo.PlaceList;
 import com.example.ria.dto.vo.PublicMapItem;
 import com.example.ria.service.map.PublicDataMapService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 @Slf4j
 @RestController
--- src/test/java/com/example/ria/controller/map/PublicDataMapControllerTest.java
+++ src/test/java/com/example/ria/controller/map/PublicDataMapControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -26,36 +25,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @DisplayName("한국 관광 공사 api test")
 class PublicDataMapControllerTest {
+
     @Autowired
     private MockMvc mvc;
-    // 테스트에 필요한 입력값 설정
-    private String mapX = "126.923885";
-    private String mapY = "37.557472";
-    private String radius = "1000";
-
-    @Test
-    @DisplayName("Open API 통신 테스트")
-    public void callOpenApi() throws Exception {
-        String mobileOS = "WIN";
-        String mobileApp = "RIA";
-        String mapX = "126.923885"; // 홍대 좌표를 기준으로 검색함.
-        String mapY = "37.557472";
-        String radius = "1000";
-        String contentTypeId = "12";
-
-
-        MultiValueMap<String, String> param = new LinkedMultiValueMap<>();
-        param.add("MobileOS", mobileOS);
-        param.add("MobileApp", mobileApp);
-        param.add("mapX", mapX);
-        param.add("mapY", mapY);
-        param.add("radius", radius);
-        param.add("contentTypeId", contentTypeId);
-
-        this.mvc.perform(MockMvcRequestBuilders.get("/api/locationBasedList1").params(param))
-                .andExpect(status().isOk())
-                .andDo(MockMvcResultHandlers.print(System.out));
-    }
 
     @Test
     @DisplayName("위치기반장소 검색")
@@ -63,6 +35,11 @@ class PublicDataMapControllerTest {
 
         MockHttpSession mockHttpSession = new MockHttpSession();
         mockHttpSession.setAttribute(Constants.SESSION_USER_ID, "USER00");
+
+        // 테스트에 필요한 입력값 설정
+        String mapX = "126.923885";
+        String mapY = "37.557472";
+        String radius = "1000";
 
         this.mvc.perform(get("/api/searchPlace")
                         .param("mapX", mapX)
